8065e1958de7cddbc9ae421cc3e8a72b8acfe2f3
Wesley Chok <chokwj@csu.fullerton.edu>
2023-10-02 17:46:26 -0700

    Removed unnecessary comments

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-// import './assets/main.css'
-
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import router from'./router';
-
-// createApp(App).mount('#app')
-
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
